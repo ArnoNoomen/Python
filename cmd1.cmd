a9707763e30340e9deea2da37da16aa3982e3805
Arno Noomen <arnonoomen@live.nl>
2022-01-02 18:45:32 +0100

    delete

---
+++ cmd1.cmd
@@ -0,0 +1,68 @@
+git checkout .gitconfig
+git checkout "1 - Introduction and First App/main.py"
+git checkout "1 - Introduction and First App/notes.txt"
+git checkout "10 - Toolbar/bottom_toolbar.py"
+git checkout "10 - Toolbar/lambda_example.py"
+git checkout "10 - Toolbar/main.py"
+git checkout "10 - Toolbar/notes.txt"
+git checkout "11 - Navigation Drawer/init.txt"
+git checkout "11 - Navigation Drawer/main.py"
+git checkout "11 - Navigation Drawer/notes.txt"
+git checkout "12 - More Navigation Drawer/init.txt"
+git checkout "12 - More Navigation Drawer/main.py"
+git checkout "12 - More Navigation Drawer/mine.jpg"
+git checkout "12 - More Navigation Drawer/navigation_drawer.py"
+git checkout "13 - Switching Screens/init.txt"
+git checkout "13 - Switching Screens/main.py"
+git checkout "2 - Labels/main.py"
+git checkout "2 - Labels/notes.txt"
+git checkout "3 - Buttons/kivy_button.py"
+git checkout "3 - Buttons/main.py"
+git checkout "3 - Buttons/notes.txt"
+git checkout "4 - Themes/main.py"
+git checkout "4 - Themes/notes.txt"
+git checkout "5 - Text Field/helpers.py"
+git checkout "5 - Text Field/main.py"
+git checkout "5 - Text Field/notes.txt"
+git checkout "6 - Binding Input and button/helpers.py"
+git checkout "6 - Binding Input and button/inputwithbutton.py"
+git checkout "7 - Dialog/__pycache__/helpers.cpython-38.pyc"
+git checkout "7 - Dialog/helpers.py"
+git checkout "7 - Dialog/main.py"
+git checkout "7 - Dialog/notes.txt"
+git checkout "8 - List/builder_basic_main.py"
+git checkout "8 - List/builder_main.py"
+git checkout "8 - List/main.py"
+git checkout "8 - List/notes.txt"
+git checkout "9 - DataTables/main.py"
+git checkout "9 - DataTables/notes.txt"
+git checkout Handheld/__pycache__/b64image.cpython-310.pyc
+git checkout Handheld/__pycache__/b64image.cpython-36.pyc
+git checkout Handheld/__pycache__/b64image.cpython-37.pyc
+git checkout Handheld/__pycache__/b64image.cpython-38.pyc
+git checkout Handheld/__pycache__/b64image.cpython-39.pyc
+git checkout Handheld/__pycache__/item.cpython-39.pyc
+git checkout Handheld/__pycache__/login.cpython-39.pyc
+git checkout Handheld/__pycache__/receiving.cpython-310.pyc
+git checkout Handheld/__pycache__/receiving.cpython-36.pyc
+git checkout Handheld/__pycache__/receiving.cpython-39.pyc
+git checkout Handheld/b64image.py
+git checkout Handheld/item.py
+git checkout Handheld/login.py
+git checkout Handheld/receiving.py
+git checkout Handheld/start.py
+git checkout __pycache__/functions.cpython-39.pyc
+git checkout android4/__pycache__/navigation_drawer.cpython-38.pyc
+git checkout android4/main.py
+git checkout android4/mine.jpg
+git checkout android4/navigation_drawer.py
+git checkout example.txt
+git checkout example2.txt
+git checkout functions.py
+git checkout laptop1
+git checkout lees.py
+git checkout login/main.py
+git checkout mail.py
+git checkout xx.txt
+git checkout yy.txt
+git checkout yyyy.ttt
