001a18af30dacbd53e2b914d34a2ace252b11bd1
Arno Noomen <arnonoomen@live.nl>
2022-01-04 16:05:33 +0100

    paul

--- MDcard/main.py
+++ MDcard/main.py
@@ -1,52 +1,51 @@
+# https://www.udemy.com/share/101Utk3@b5jRGn9gJYVO0W8fzcUs9PRBuitpji58aINb5YJXJzOb6RlH6nzZj--5UWU2bqhf/
+
+# https://kivymd.readthedocs.io/en/latest/components/dialog/
+
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
+from kivymd.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
+import runpy
+
 # set window size
-Window.size=(300,450)
+Window.size=(310,450)
 
 KV='''
 Screen:
     
-    MDCard:
-        size_hint: None, None
-        size: 300, 450
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
-        elevation: 10
-        padding: 65
-        spacing: 35
-        orientation: 'vertical'
-        MDTextField:
-            id: article
-            icon_left: "account-check"
-            hint_text: "Article"
-            foreground_color: 1, 0, 1, 1
-            size_hint_x: None
-            width: 220
-            font_size: 20
-            pos_hint: {"center_x": 0.5}
-        MDTextField:
-            id: dock
-            icon_left: "key-variant"
-            hint_text: "Password"
-            foreground_color: 1, 0, 1, 1
-            size_hint_x: None
-            width: 220
-            font_size: 20            
-            pos_hint: {"center_x": 0.5}
-            password: True
-        MDFillRoundFlatButton:
-            text: "Ok"
-            font_size: 15
-            # pos_hint: {"left_x": 0.5}
-            pos:(1,1)
-            on_press: app.login()
-        MDFillRoundFlatButton:
-            text: "Cancel"
-            font_size: 15
-            pos:(5,1)
-            on_press: app.login() 
+    MDTextField:
+        pos: 1,360
+        id: receive
+        icon_left: "account-check"
+        hint_text: "Receive Number"
+        foreground_color: 1, 0, 1, 1
+        width: 220
+        font_size: 20   
+       
+    MDTextField:
+        pos: 1,100
+        id: dock
+        icon_left: "key-variant"
+        hint_text: "Dock"
+        foreground_color: 1, 0, 1, 1
+        width: 220
+        font_size: 20         
+    
+    MDFillRoundFlatButton:
+        text: "Ok"
+        font_size: 15    
+        orientation: 'horizontal'
+        on_press: app.verder()
+        pos: 1,1
+    MDFillRoundFlatButton:
+        text: "Cancel"
+        font_size: 15
+        orientation: 'horizontal'
+        pos: 100,1
+        # on_press: app.login() 
 '''
 
 
@@ -60,25 +59,14 @@ class LoginApp(MDApp):
         # load and return kv string
         return Builder.load_string(KV)
     
-    def login(self):
-        # check entered username and password
-        if self.root.ids.user.text=='admin' and self.root.ids.password.text=='admin123':
-            if not self.dialog:
-                # create dialog
-                self.dialog = MDDialog(
-                    title="Log In",
-                    text=f"Welcome {self.root.ids.user.text}!",
-                    buttons=[
-                        MDFlatButton(
-                            text="Ok", text_color=self.theme_cls.primary_color,
-                            on_release=self.close
-                        ),
-                    ],
-                )
-            # open and display dialog
-            self.dialog.open()
+    def verder(self):
+        print( 'jaja' )
+        self.dialog.dismiss()
+        runpy.run_module(mod_name='banner')
+
     def close(self, instance):
         # close dialog
         self.dialog.dismiss()
+    
 # run app    
 LoginApp().run()
