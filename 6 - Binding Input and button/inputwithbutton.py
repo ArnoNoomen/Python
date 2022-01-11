c5e2789ff278d6cf0041a3285abe43d4a2581ddc
jphnoomen <jphnoomen@gmail.com>
2022-01-11 13:09:34 +0100

    bol program

--- 6 - Binding Input and button/inputwithbutton.py
+++ 6 - Binding Input and button/inputwithbutton.py
@@ -2,20 +2,31 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDRectangleFlatButton
 from kivy.lang import Builder
-import helpers
 
+
+username_input = """
+MDTextField:
+    id: username
+    hint_text: "Enter username"
+    helper_text: "or click on forgot username"
+    helper_text_mode: "on_focus"
+    icon_right: "account"
+    icon_right_color: app.theme_cls.primary_color
+    pos_hint:{'center_x': 0.5, 'center_y': 0.5}
+    size_hint: 0.9 , 0.1
+    width: 100
+"""
 
 class DemoApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Green"
         screen = Screen()
 
-        self.username = Builder.load_string(helpers.username_input)
+        self.username = Builder.load_string(username_input)
         button = MDRectangleFlatButton(text='Show',
                                        pos_hint={'center_x': 0.5, 'center_y': 0.5},
                                        on_release=self.show_data)
         screen.add_widget(self.username)
-        screen.add_widget(button)
         return screen
 
     def show_data(self,obj):
