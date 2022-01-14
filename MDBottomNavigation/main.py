c91c7b5fa914f6a415c052e798b3431b8e200303
Arno Noomen <arnonoomen@live.nl>
2022-01-14 22:14:15 +0100

    MDBottomNavigation

--- MDBottomNavigation/main.py
+++ MDBottomNavigation/main.py
@@ -1,6 +1,14 @@
 from kivymd.app import MDApp
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
+popup = Popup(title='Test popup',
+    content=Label(text='Hello world'),
+    size_hint=(None, None), size=(400, 400)
+)
 class MainApp(MDApp):
-    pass
+
+    def popup1(self):
+        popup.open()
 
 MainApp().run()
