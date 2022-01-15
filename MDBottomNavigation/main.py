8e3aa438890a68b099012c56b39223f771b72c30
Arno Noomen <arnonoomen@live.nl>
2022-01-15 20:58:56 +0100

    oke

--- MDBottomNavigation/main.py
+++ MDBottomNavigation/main.py
@@ -3,6 +3,8 @@ from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelOneLine
 from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 
 class MyContent(BoxLayout):
     pass
@@ -12,9 +14,29 @@ popup = Popup(title='Test popup',
     size_hint=(None, None), size=(400, 400)
 )
 class MainApp(MDApp):
+    dialog = None
 
     def popup1(self):
         popup.open()
+
+    def show_alert_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                text="Discard draft?",
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                    ),
+                    MDFlatButton(
+                        text="DISCARD",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                    ),
+                ],
+            )
+        self.dialog.open()
 
     def on_start(self):
         names = ["Option 1", "Option 2", "Option 3"]
