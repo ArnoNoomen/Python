4c4dcfd020051a2a32a69c5f37139eabc62d6071
arno noomen <arnonoomen@ziggo.nl>
2022-01-15 18:30:54 +0100

    oke

--- MDBottomNavigation/main.py
+++ MDBottomNavigation/main.py
@@ -1,6 +1,11 @@
 from kivymd.app import MDApp
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
+from kivymd.uix.expansionpanel import MDExpansionPanel
+from kivy.uix.boxlayout import BoxLayout
+
+class MyContent(BoxLayout):
+    pass
 
 popup = Popup(title='Test popup',
     content=Label(text='Hello world'),
@@ -10,5 +15,12 @@ class MainApp(MDApp):
 
     def popup1(self):
         popup.open()
+
+    def on_start(self):
+        names = ['Option 1', 'Option 2', , 'Option 3']
+
+        for name in names:
+            panel = MDExpansionPanel(title=name, content=MyContent())
+            self.root.ids.panel_container.add_widget(panel)
 
 MainApp().run()
