e3eb9e09909e08d1cf9a98e73d0c87e4ecf1688f
Arno Noomen <arnonoomen@live.nl>
2022-01-19 19:51:26 +0100

    oke

--- MDBottomNavigation/main.py
+++ MDBottomNavigation/main.py
@@ -32,8 +32,9 @@ class MDTextFieldPersian(MDTextField):
 
 class MainApp(MDApp):
     dialog = None
-    dropdown = None  
-      
+    dropdown = None 
+     
+
 
     def show_data(self): 
         oke_button = MDFlatButton(text='Oke', on_release=self.close_dialog)
