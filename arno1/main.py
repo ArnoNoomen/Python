af5be9b86e438df549fc775cd1be5bf74d495039
Arno Noomen <arnonoomen@live.nl>
2022-01-26 16:31:01 +0100

    oke

--- arno1/main.py
+++ arno1/main.py
@@ -1,3 +1,4 @@
+from typing_extensions import Self
 from kivymd.app import MDApp
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
@@ -6,16 +7,18 @@ from kivymd.uix.screen import Screen
 class MainApp(MDApp):
     def show_data(*args):
         oke_button = MDFlatButton(text='Oke',on_press=MainApp.close_dialog)
-        MainApp.mydialog = MDDialog(text='Helloworld', size_hint=(0.7, 1), buttons=[oke_button])
+        MainApp.mydialog = MDDialog(text=f'{MainApp.screen.width} {MainApp.screen.height}',
+                                    size_hint=(0.7, 1), buttons=[oke_button])
+        MainApp.mydialog.text = MainApp.mydialog.text + f'\n{MainApp.mydialog.width} {MainApp.mydialog.height}'
         MainApp.mydialog.open()
     def close_dialog(*args):
         MainApp.mydialog.dismiss()
     def build(self):
-        screen = Screen()
+        MainApp.screen = Screen()
         btn1 = MDFlatButton(text='Oke',
                             pos_hint={'center_x': 0.5, 'center_y': 0.5},
                             on_press=self.show_data)
-        screen.add_widget(btn1)
-        return screen
+        MainApp.screen.add_widget(btn1)
+        return MainApp.screen
 
 MainApp().run()
