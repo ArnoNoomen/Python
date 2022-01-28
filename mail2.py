591b6308f5c122fb43b9da382a3e275b9b5dccb9
Arno Noomen <arnonoomen@live.nl>
2022-01-28 11:47:26 +0100

    mail2

--- mail2.py
+++ mail2.py
@@ -12,6 +12,7 @@ from email.mime.image import MIMEImage
 from email.mime.base import MIMEBase
 
 # https://support.microsoft.com/en-us/topic/how-to-send-an-email-with-an-embedded-image-879d3e13-6bc4-6c61-417a-b7419d304d52
+# https://code.activestate.com/recipes/473810/
 
 def main():
 
@@ -27,36 +28,30 @@ def main():
     except FileNotFoundError:
         print('testbestanden/smtp.json is niet aanwezig')
         sys.exit(1)
-    attach_dict = {}
-    try:
-        with open("testbestanden/attach.json", encoding='iso-8859-1') as fp1:
-            attach_dict = json.loads(fp1.read())
-    except FileNotFoundError:
-        pass
 
-    msgRoot = MIMEMultipart('related')
-    msgRoot['Subject'] = args.subject
-    msgRoot['From'] = smtp_dict['from']
-    msgRoot['To'] = args.to
+    msgroot = MIMEMultipart('related')
+    msgroot['Subject'] = args.subject
+    msgroot['From'] = smtp_dict['from']
+    msgroot['To'] = args.to
 
-    msgRoot.preamble = 'This is a multi-part message in MIME format.'
-    msgAlternative = MIMEMultipart('alternative')
-    msgRoot.attach(msgAlternative)
+    msgroot.preamble = 'This is a multi-part message in MIME format.'
+    msgalternative = MIMEMultipart('alternative')
+    msgroot.attach(msgalternative)
     #msgText = MIMEText('This is the alternative plain text message.')
     #msgAlternative.attach(msgText)
-    msgText = MIMEText('<img src="cid:image1">', 'html')
-    msgAlternative.attach(msgText)
-    fp = open('testbestanden/Knipsel.PNG', 'rb')
-    msgImage = MIMEImage(fp.read())
-    fp.close()
-    msgImage.add_header('Content-ID', '<image1>')
-    msgRoot.attach(msgImage)
+    msgtext = MIMEText('<img src="cid:image1">', 'html')
+    msgalternative.attach(msgtext)
+    fp1 = open('testbestanden/Knipsel.PNG', 'rb')
+    msgimage = MIMEImage(fp1.read())
+    fp1.close()
+    msgimage.add_header('Content-ID', '<image1>')
+    msgroot.attach(msgimage)
 
     try:
         server = smtplib.SMTP(smtp_dict['server'], smtp_dict['port'])
         server.starttls()
         server.login(smtp_dict['from'], smtp_dict['pwd'])
-        server.send_message(msgRoot)
+        server.send_message(msgroot)
         server.quit()
     except:
         print('fout met verzenden')
