f6b172798900bfb8fdabfbfa26db25ad9496a9fd
Arno Noomen <arnonoomen@live.nl>
2022-02-01 12:20:38 +0100

    oke

--- arno2/main.py
+++ arno2/main.py
@@ -6,6 +6,9 @@ from kivymd.app import MDApp
 from kivymd.uix.list import OneLineListItem
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
+sys.path.insert(1, '../testbestanden')
+sys.path.insert(2, 'D:\\Github\\Python\\testbestanden')
+import variabelen
 
 # https://kivy.org/doc/stable/api-kivy.network.urlrequest.html
 
@@ -22,7 +25,14 @@ def got_success(*args):
             item = OneLineListItem(text=rij['name'])
             MainApp.screen1.ids.container.add_widget(item)
     except:
-        pass
+        try:
+            print(args[1]['error'])
+        except:
+            pass
+        oke_button = MDFlatButton(text='Oke',on_press=close_dialog)
+        MainApp.mydialog = MDDialog(text='Fout',
+                                    size_hint=(0.7, 1), buttons=[oke_button])
+        MainApp.mydialog.open()
 
 def got_error(*args):
     oke_button = MDFlatButton(text='Oke',on_press=close_dialog)
@@ -53,20 +63,11 @@ class MainApp(MDApp):
         return MainApp.screen1
 
     def on_start(self):
-        webrequest_dict = {}
-        try:
-            with open("../testbestanden/webrequest.json", encoding='iso-8859-1') as fp1:
-                webrequest_dict = json.loads(fp1.read())
-        except FileNotFoundError:
-            print('testbestanden/webrequest.json is niet aanwezig')
-            sys.exit(1)
-
-        myheader = {'X-Oc-Merchant-Id': '123'}
-        UrlRequest(url='http://' + webrequest_dict["site"] + webrequest_dict["pagina"],
+        UrlRequest(url=variabelen.url1,
                         on_success=got_success,
                         on_failure=got_failure,
                         on_error=got_error,
                         on_progress=got_progress,
-                        req_headers=myheader)
+                        req_headers=variabelen.header1)
 
 MainApp().run()
