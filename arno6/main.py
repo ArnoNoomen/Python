aed69f0aa4d5cdd4ce94458012b79b834582fdf7
Arno Noomen <arnonoomen@live.nl>
2022-02-09 11:31:39 +0100

    arno6

--- arno6/main.py
+++ arno6/main.py
@@ -4,6 +4,7 @@ from kivy.metrics import dp
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 from kivymd.uix.datatables import MDDataTable
+from kivymd.uix.button import MDFlatButton
 
 class MainApp(MDApp):
 
@@ -26,14 +27,20 @@ class MainApp(MDApp):
         )
         MainApp.mytable.bind(on_check_press=self.checked)
         MainApp.mytable.bind(on_row_press=self.row_checked)
+        oke_button = MDFlatButton(text='Oke',on_press=MainApp.addrow)
+
         screen.add_widget(MainApp.mytable)
+        screen.add_widget(oke_button)
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "BlueGray"
         return screen
 
-    def checked(self, instance_table, current_row):
+    def addrow(*args):
         rij = ("a2","b","c","d")
         MainApp.mytable.row_data.append(rij)
+
+    def checked(self, instance_table, current_row):
+        print(instance_table)
 
     def row_checked(self, instance_table, instance_row):
         pass
