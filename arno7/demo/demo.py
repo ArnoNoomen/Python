54b97dacb5b31b20f7037bb35fb984cfd09d4dae
Arno Noomen <arnonoomen@live.nl>
2022-02-24 11:18:19 +0100

    arno7

--- arno7/demo/demo.py
+++ arno7/demo/demo.py
@@ -1,5 +1,31 @@
 chats = {
-    "jordan": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"]
+    "anita": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"],
+    "jooyce": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"],
+    "weeknd": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"],
+    "jordan": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"],
+    "breezy": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"],
+    "wizkid": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"],
+    "davido": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"],
+    "zendaya": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"],
+    "rihanna": ["hey;2:20 PM;read;you","hi whats up;2:20 PM;read;friend"]
+}
+anita = {
+    "name": "Anita",
+    "image": "assets/images/anita.jpg",
+    "active": False,
+    "msg": chats["anita"]
+}
+jooyce = {
+    "name": "Jordan",
+    "image": "assets/images/16.jpg",
+    "active": False,
+    "msg": chats["jooyce"]
+}
+weeknd = {
+    "name": "Jordan",
+    "image": "assets/images/weeknd.jpg",
+    "active": True,
+    "msg": chats["weeknd"]
 }
 jordan = {
     "name": "Jordan",
@@ -7,5 +33,34 @@ jordan = {
     "active": False,
     "msg": chats["jordan"]
 }
-
-profiles = [jordan]
+breezy = {
+    "name": "Breezy",
+    "image": "assets/images/breezy.jpg",
+    "active": False,
+    "msg": chats["breezy"]
+}
+wizkid = {
+    "name": "Wizkid",
+    "image": "assets/images/wizkid.jpg",
+    "active": False,
+    "msg": chats["wizkid"]
+}
+davido = {
+    "name": "Davido",
+    "image": "assets/images/davido.jpg",
+    "active": False,
+    "msg": chats["davido"]
+}
+zendaya = {
+    "name": "Zendaya",
+    "image": "assets/images/zendaya.jpg",
+    "active": False,
+    "msg": chats["zendaya"]
+}
+rihanna = {
+    "name": "Rihanna",
+    "image": "assets/images/rihanna.jpg",
+    "active": False,
+    "msg": chats["rihanna"]
+}
+profiles = [anita, jooyce, weeknd, jordan, breezy, wizkid, davido, zendaya, rihanna]
